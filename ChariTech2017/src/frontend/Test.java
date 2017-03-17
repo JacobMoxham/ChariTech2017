6b3442a9dda208b02e3f8023f000490f20a796d5
Samuel Silvester <pighead10@hogpog.co.uk>
2017-03-17 14:02:31 +0000

    initial commit

---
+++ ChariTech2017/src/frontend/Test.java
@@ -0,0 +1,8 @@
+package frontend;
+
+/**
+ * Created by pighe on 17/03/2017.
+ */
+public class Test {
+
+}
--- ChariTech2017/src/slate2learn/Application.java
+++ ChariTech2017/src/slate2learn/Application.java
@@ -1,8 +1,8 @@
 package slate2learn;
 
 public class Application {
-	public static void main(String args[]){
-		
-	}
-	
+    public static void main(String args[]){
+        System.out.println("Hello world!");
+    }
+
 }

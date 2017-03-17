4232cac6d79a081d8f3968167887450c1f737927
alicja <alicja@alicja-ThinkPad-T450s>
2017-03-17 14:17:00 +0000

    :)

--- ChariTech2017/src/datastatistics/Tutor.java
+++ ChariTech2017/src/datastatistics/Tutor.java
@@ -1,13 +1,45 @@
 package datastatistics;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Tutor {
 	
 	private Integer centre_id;
 	
+	Tutor (Integer id)
+	{
+		centre_id = id;
+	}	
 	
+	//last month
+	Double getBusinessData (Statistic name)
+	{
+		
+	}
 	
+	//last months - index 0 - the most recent month
+	List<Double> getAllBusinessData (Statistic name)
+	{
+		
+	}
+	Double getAdministrativeData (Statistic name)
+	{
+		
+	}
+	
+	List<Double> getAllAdministrativeData (Statistic name)
+	{
+		
+	}
+	Double getLearningData (Statistic name)
+	{
+		
+	}
+	List<Double> getAllLearningData (Statistic name)
+	{
+		
+	}
 	
 
 }

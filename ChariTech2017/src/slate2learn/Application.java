8bbbd5b3eac3a03955db180731dec17eaf4b8c18
Jacob Moxham <jacobmoxham@live.co.uk>
2017-03-17 13:05:26 +0000

    Added basic methods (empty) to application

--- ChariTech2017/src/slate2learn/Application.java
+++ ChariTech2017/src/slate2learn/Application.java
@@ -1,5 +1,17 @@
 package slate2learn;
+import processing.core.*;
 
-public class Application {
-
+public class Application extends PApplet{
+	public static void main(String args[]){
+		PApplet.main("Application");
+	}
+	public void settings(){
+		
+	}
+	public void setup(){
+		
+	}
+	public void draw(){
+		
+	}
 }

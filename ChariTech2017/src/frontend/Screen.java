21a68d26122f17f51f8b860648e2da18ece72478
Jacob Moxham <jacobmoxham@live.co.uk>
2017-03-17 18:32:59 +0000

    Started writing the method to draw buttons for a set of metrics

--- ChariTech2017/src/frontend/Screen.java
+++ ChariTech2017/src/frontend/Screen.java
@@ -2,6 +2,7 @@ package frontend;
 
 import com.sun.javafx.applet.Splash;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 /**
@@ -26,6 +27,38 @@ public abstract class Screen {
 
     protected Main getParent(){
         return mParent;
+    }
+    
+    protected void drawMetricButtons(List<Metric> metrics){
+    	GridPane gridPane = new GridPane();
+    	RowConstraints titleRow = new RowConstraints();
+    	titleRow.setPercentHeight(0.2);
+    	RowConstraints metricsRow = new RowConstraints();
+    	titleRow.setPercentHeight(0.8);
+    	
+    	//Calculate measure of how good each metric is compared to the others
+    	double total=0;
+    	for (Metric metric: Metrics){
+    		total+= metric.getValue();
+    	}
+    	
+    	//Draw metric buttons
+    	int i = 0;
+    	for (Metric metric: metrics){
+    		//TODO: fine tune button sizes
+    		CircleButton metBut = new CircleButton(metric.getValue(), 50 + (metric.getValue()/total)*200);
+    		//Handle clicks
+    		if (metric.thatBooleanThing){
+    			metBut.setOnAction(e -> createMetricScreen(metric.getTimeSensitiveMetrics));
+    		}
+    		gridPane.add(metBut, i, 1);
+    		i++;
+    	}
+    	//TODO: make sure this stuff gets drawn
+    	
+    }
+    protected void createMetricScreen(List<Metric> timeMetrics){
+    	
     }
 
     protected abstract void generateScene();

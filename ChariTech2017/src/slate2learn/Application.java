93b5519be108ea6fce26f0b8651b49468429fe55
Jacob Moxham <jacobmoxham@live.co.uk>
2017-03-17 13:27:27 +0000

    Removed processing stuff

--- ChariTech2017/src/slate2learn/Application.java
+++ ChariTech2017/src/slate2learn/Application.java
@@ -1,17 +1,8 @@
 package slate2learn;
-import processing.core.*;
 
-public class Application extends PApplet{
+public class Application {
 	public static void main(String args[]){
-		PApplet.main("Application");
-	}
-	public void settings(){
 		
 	}
-	public void setup(){
-		
-	}
-	public void draw(){
-		
-	}
+	
 }

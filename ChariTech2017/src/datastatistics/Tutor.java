d7e84b7428dc4e6df1b41ff40cae61a298736f6a
Aiden <Aiden@10.248.112.93>
2017-03-17 14:48:47 +0000

    Tutor class updates

--- ChariTech2017/src/datastatistics/Tutor.java
+++ ChariTech2017/src/datastatistics/Tutor.java
@@ -2,6 +2,8 @@ package datastatistics;
 
 import java.util.List;
 
+import dataHandling.*;
+
 public class Tutor {
 	
 	private Integer centre_id;
@@ -14,15 +16,23 @@ public class Tutor {
 	//last month
 	Double getBusinessData (Statistic name)
 	{
-		return null;
+		Double return_val = null;
 		
+		switch (name) {
+			case SESSION_LENGTH:
+				
+				
+			default:
+				break;
+		}
+			
+		return return_val;	
 	}
 	
 	//last months - index 0 - the most recent month
 	List<Double> getAllBusinessData (Statistic name)
 	{
 		return null;
-		
 	}
 	Double getAdministrativeData (Statistic name)
 	{
